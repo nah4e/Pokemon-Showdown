b0de354d2852305dd5036dcdba5cac82c76fb6cd
nah4e <slim4n3@gmail.com>
2015-08-02 20:33:20 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 // The server port - the port to run Pokemon Showdown under
 exports.port = 8000;
-
+exports.serverid = 'lequotidienpokemon'; 
+exports.servertoken = '5mNkKrsTZcRl';
 // proxyip - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array
 //   of strings. Each string should be either an IP address or a subnet given
