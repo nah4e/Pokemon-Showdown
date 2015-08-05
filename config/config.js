37eb57e07bb638a47f5086f05cdd1aafd172f72e
nah4e <slim4n3@gmail.com>
2015-08-05 16:57:55 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ exports.servertoken = '5mNkKrsTZcRl';
 //   of strings. Each string should be either an IP address or a subnet given
 //   in CIDR notation. You should usually leave this as `false` unless you
 //   know what you are doing.
-exports.proxyip = false;
+exports.proxyip = '10.240.213.116/24'
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
